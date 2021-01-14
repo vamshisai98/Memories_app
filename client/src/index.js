ec927174917d12a8948546652ee6531091f80983
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-01-14 22:29:51 +0530

    layout updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+
 import App from './App';
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+  <React.StrictMode></React.StrictMode>,
   document.getElementById('root')
 );
