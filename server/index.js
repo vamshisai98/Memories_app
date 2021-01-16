b683dd378530baee205ecba7ffdd9035546cd2b7
vamshisai98 <42145575+vamshisai98@users.noreply.github.com>
2021-01-16 12:07:18 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -13,12 +16,17 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL =
-  'mongodb+srv://training:7KysVUkkpVP7oX00@db-training-cluster.nsjyl.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+  res.send('hello to memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
